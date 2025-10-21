fe01689bb8642e7447786bbe1053911e8e842991
khalloufiajroureda <khalloufiajroureda001.alu@stemgranada.com>
2025-10-21 11:42:05 +0200

    ACTUALIZACION

---
+++ Base de datos/base de datos.sql
@@ -0,0 +1,7 @@
+insert into COCHES(marca,modelo,matricula, precio,IDdueno)
+values
+('Toyota', 'Corolla', '1234ABC', 15000, 1),
+('Honda', 'Civic', '5678DEF', 18000, 2),
+('Ford', 'Focus', '9101GHI', 16000, 3),
+('Chevrolet', 'Malibu', '1121JKL', 20000, 4),
+('Nissan', 'Sentra', '3141MNO', 17000, 5);
