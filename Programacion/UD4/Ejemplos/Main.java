23eb91fd2f19baa65d1071f00747dff5c78fd6a3
khalloufiajroureda <khalloufiajroureda001.alu@stemgranada.com>
2026-01-08 09:30:55 +0100

    ejemplos java

---
+++ Programacion/UD4/Ejemplos/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Introduce tu nombre: ");
+        String nombre = scanner1.nextLine();
+        System.out.println("Nombre: " + nombre);
+    }
+}
