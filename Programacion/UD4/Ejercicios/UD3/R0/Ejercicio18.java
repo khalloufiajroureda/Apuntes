5156fa6cef2a186b5e36359c16b0683837804b4d
khalloufiajroureda <khalloufiajroureda001.alu@stemgranada.com>
2026-01-16 11:40:56 +0100

    ejercicios java

---
+++ Programacion/UD4/Ejercicios/UD3/R0/Ejercicio18.java
@@ -0,0 +1,20 @@
+package Programacion.UD4.Ejercicios.UD3.R0;
+
+public class Ejercicio18 {
+    
+    public static int contarVocalesEnCadena(String cadenaTexto){
+        cadenaTexto = cadenaTexto.toUpperCase();
+        int cantidadTotalVocales = 0;
+        for (char letra : cadenaTexto.toCharArray()) {
+            if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
+                cantidadTotalVocales++;
+            }
+        }
+        return cantidadTotalVocales;
+    }
+
+    public static void main(String[] args) {
+        final String CADENA = "REDA";
+        System.out.println(contarVocalesEnCadena(CADENA));
+    }
+}
