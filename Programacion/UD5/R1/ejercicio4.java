ec77bdd35734d3524205bee9f49d641d8126c7e9
khalloufiajroureda <khalloufiajroureda001.alu@stemgranada.com>
2026-02-02 11:38:51 +0100

    ejercicios

---
+++ Programacion/UD5/R1/ejercicio4.java
@@ -0,0 +1,24 @@
+package Programacion.UD5.R1;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class Auto {
+    String marca;
+    String modelo;
+    int velocidadActual;
+
+    Auto(String marca, String modelo, int velocidadActual) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadActual = velocidadActual;
+   }
+
+   int acelerar(){
+    
+   }
+}
