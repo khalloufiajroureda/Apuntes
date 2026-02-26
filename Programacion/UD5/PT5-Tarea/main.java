37ef527ead861ca4ae5bfdfa4c124e8cd444e799
khalloufiajroureda <khalloufiajroureda001.alu@stemgranada.com>
2026-02-26 22:13:30 +0100

    tarea pt5

---
+++ Programacion/UD5/PT5-Tarea/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        Animal leon = new Animal("LEON", "A0001", 3, 27);
+
+        System.out.println(leon);
+
+
+    }
+}
--- Programacion/UD5/R2/Ejercicio4/Playlist.java
+++ Programacion/UD5/R2/Ejercicio4/Playlist.java
@@ -1,41 +1,4 @@
-import java.util.ArrayList;
 
-public class Playlist {
-
-    ArrayList<Cancion> canciones;
-
-    public Playlist(ArrayList<Cancion> canciones) {
-        this.canciones = canciones;
-    }
-
-    public Playlist() {
-        canciones = new ArrayList<>();
-    }
-
-    public Playlist(String titulo, String artista, int duracion) {
-        Cancion cancion1 = new Cancion(titulo, artista, duracion);
-        agregarCancion(cancion1);
-    }
-
-    public void agregarCancion(Cancion cancion) {
-        canciones.add(cancion);
-    }
-
-    public double calcularDuracionPlaylist() {
-        double duracionTotal = 0;
-        for (int i = 0; i < canciones.size(); i++) {
-            duracionTotal += canciones.get(i).getDuracion();
-        }
-        return duracionTotal;
-    }
-
-    @Override
-    public String toString() {
-        String inventario = "";
-        for (int i = 1; i <= canciones.size(); i++) {
-            inventario += "Playlist --> " + i + ": " + canciones.get(i - 1) + "\n";
-        }
-        return inventario;
-    }
+public class PlayList {
 
 }
